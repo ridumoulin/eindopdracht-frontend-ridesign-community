259e61967abcabcb63edd782a0a1b905f993a28a
RiDumoulin <riannedumoulin@gmail.com>
2024-02-07 15:35:37 +0100

    Opzet routing structuur

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { Routes, Route, Navigate } from 'react-router-dom';
 
 function App() {
-  const [count, setCount] = useState(0)
-
+    const isLoggedIn = false;
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+          <Navigation />
+          <Routes>
+              <Route path="/" element={<Home />}/>
+              <Route path="/OverviewProducts" element={<OverviewProducts />}/>
+              <Route path="/Products" element={<Products />}/>
+              <Route path="/NewProduct" element={<NewProduct />}/>
+              <Route path="/SignIn" element={<SignIn />}/>
+              <Route path="/SignUp" element={<SignUp />}/>
+              <Route path="/Profile" element={isLoggedIn === true ? <LogIn/> : <Navigate to="/"/>}/>
+              <Route path="/Favorites" element={<Favorites />}/>
+              <Route path="/ShoppinCart" element={<ShoppingCart />}/>
+              <Route path="/Info" element={<Info />}/>
+              <Route path="/Inquiries" element={<Inquiries />}/>
+              <Route path="/Error" element={<Error />}/>
+          </Routes>
+          <Footer />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
