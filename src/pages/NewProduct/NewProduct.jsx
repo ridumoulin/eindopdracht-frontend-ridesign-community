3c827bc90c3638572a16aeacd394aaca80b45d4c
RiDumoulin <riannedumoulin@gmail.com>
2024-02-13 10:19:13 +0100

    NewProduct formulier verder gewerkt

--- src/pages/NewProduct/NewProduct.jsx
+++ src/pages/NewProduct/NewProduct.jsx
@@ -81,6 +81,90 @@ function NewProduct() {
                 </div>
 
 
+                <label htmlFor="category-select">
+                    Categorie
+                    <div className="dropdown">
+                        <select
+                            id="category-select"
+                            {...register("categories")}
+                            multiple
+                            className="category-dropdown"
+                            placeholder="selecteer een categorie"
+
+                        >
+                            {/*<option value="">--Selecteer categorie--</option>*/}
+                            <option value="beds">Banken</option>
+                            <option value="tables">Bedden</option>
+                            <option value="sofas">Kasten</option>
+                            <option value="chairs">Stoelen & fauteuils</option>
+                            <option value="tables">Tafels</option>
+                            <option value="garden-furniture">Tuinmeubelen</option>
+                        </select>
+                    </div>
+                </label>
+
+                <label htmlFor="dimensions-field">
+                    Afmetingen (lxbxh)
+                    <input
+                        type="text"
+                        id="dimensions-field"
+                        {...register("dimensions")}
+                        className="input-form"
+                    />
+                    {errors.dimensions && <span className="error-message">De afmetingen zijn vereist.</span>}
+                </label>
+
+                <label htmlFor="message-field">
+                    Geef een omschrijving
+                    <textarea
+                        id="message-field"
+                        {...register("message")}
+                        className="input-form"
+                    />
+                    {errors.message && <span className="error-message">Een omschrijving is vereist.</span>}
+                </label>
+
+                <label htmlFor="materials-field">
+                    Materialen
+                    <input
+                        type="text"
+                        id="materials-field"
+                        {...register("materials")}
+                        className="input-form"
+                    />
+                    {errors.materials && <span className="error-message">Het aangeven van materialen is vereist.</span>}
+                </label>
+
+                <div className="checkbox-container">
+                    <div className="content-checkbox">
+                        <label htmlFor="delivery-checkbox" className="checkbox-label">Bezorgen</label>
+                        <input
+                            type="checkbox"
+                            id="delivery-checkbox"
+                            {...register("delivery")}
+                            value="bezorgen"
+                            className="checkbox"
+                        />
+                    </div>
+
+                    <div className="content-checkbox">
+                        <label htmlFor="pickup-checkbox" className="checkbox-label">Ophalen</label>
+                        <input
+                            type="checkbox"
+                            id="pickup-checkbox"
+                            {...register("pickup")}
+                            value="ophalen"
+                            className="checkbox"
+                        />
+                    </div>
+
+                    {errors.delivery && <span className="error-message">Het aangeven van de leveringsoptie is vereist.</span>}
+                </div>
+
+                <button type="submit">
+                    RiDesign toevoegen
+                </button>
+
             </form>
         </div>
     )
